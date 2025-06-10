c83aa67877d4a602f7b8f508305da2c3478d7eec
BrendDev-arch <brendhonluccas@icloud.com>
2025-06-10 11:40:34 -0300

    Finalizado

--- src/Kalango.java
+++ src/Kalango.java
@@ -2,7 +2,61 @@ public class Kalango extends Personagem{
 
     @Override
     public void mover() {
-        System.out.println("Terrestre com velocidade");
+        System.out.println(" O Kalango Trabalhando: " + "\n");
+        int energy = super.getEnergia();
+        boolean continuar = true;
+        energy = 12;
+        if (continuar) { 
+            while (energy > 4) { 
+                continuar = false;
+                energy = energy - 3;
+                System.out.println("Movendo.... Energia: "+ energy + "\n");
+            }
+        }
+    }
+        
+    @Override
+    public void fazerSom() {
+        System.out.println( "Fazendo som: Tssss!");
+        
     }
 
+    @Override
+    public void dormir(int energia) {    
+        super.dormir(energia);
+        boolean continuar = true;
+        if(continuar) {  
+            while (energia < 25){
+                continuar = false;
+                energia = energia + 5; 
+            }
+            System.out.println("Dormindo... Energia: " + energia);      
+        }
+    }
+    public void comer() {
+        int energiaComendo = super.getEnergia();
+        energiaComendo = 25;
+        if (energiaComendo <= 45 ) {
+            for(int i = 0 ; i <= 10 ; i++){
+                energiaComendo++;  
+            }
+            System.out.println("Comendo... Energia: " + energiaComendo);
+        }
+    }
+    @Override
+    public void cansado() {
+        int energy = super.getEnergia();
+        energy = 5;            
+        if (energy <= 5 ){
+            for(int i = 0 ; i <= 2 ; i++){
+                energy--;  
+            }
+            System.out.println("cansada... Energia: " + energy);
+        }
+    } 
+
+    public void setNome(String nome){
+        super.setNome(nome);
+        System.out.println(super.getNome());     
+    }
 }
--- src/Personagem.java
+++ src/Personagem.java
@@ -1,18 +1,31 @@
 public abstract class Personagem {
     protected int energia;
 
+    private String nome;
+
     public abstract void mover();
 
     public abstract void fazerSom();
+
+    public abstract void comer();
+
+    public abstract void cansado();
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
 
     public void dormir(int energia){
         this.energia = energia;
     }
 
-    public int getEnergia() {
-        energia+=10;
+    public int getEnergia(){
         return energia;
     }
 
-    
 }
